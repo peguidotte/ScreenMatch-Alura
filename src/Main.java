4174da86d95e960f9882a8727e536207692c0535
PedroGuidotte <peguidotte@gmail.com>
2024-07-02 20:55:18 -0300

    add (set and get methods)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
+import peguidotte.screenmatch.modelos.Film;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Film test1 = new Film();
-        test1.name = "Deadpool";
-        test1.description = "A anti-hero and comedian, dress with a red uniform, two swords, two guns and a lot of bullshits. Oh, he is immortal to.";
-        test1.date = 2016;
-        test1.minutes = 108;
-        test1.yearClassification = 18;
-        test1.planInclude = true;
-        test1.totalScore = 987.8;
-        test1.scoreAmount = 100;
-        test1.protagonist = "Ryan Reynolds";
-        test1.director = "Tim Miller";
-        test1.genre = "Action and Comedy";
+        Film test2 = new Film();
+        test2.setName("Deadpool");
+        test2.setDescription("A anti-hero and comedian, dress with a red uniform, two swords, two guns and a lot of bullshits. Oh, he is immortal to.");
+        test2.setDate(2016);
+        test2.setMinutes(108);
+        test2.setYearClassification(18);
+        test2.setPlanInclude(true);
+        test2.addScore(9);
+        test2.addScore(5);
+        test2.addScore(9);
+        test2.setProtagonist("Ryan Reynolds");
+        test2.setDirector("Tim Miller");
+        test2.setGenre("Action and Comedy");
 
-        test1.showMainInformations();
+        //test
+        System.out.println("Film name: " + test2.getName());
+        System.out.println("Score: " + test2.averageScore());
+        System.out.println("+" + test2.getYearClassification());
+        System.out.println("Launch: " + test2.getDate());
+        System.out.println("Time: " + test2.getMinutes() + " minutes");
+        System.out.println(test2.getDescription());
+        if (test2.isPlanInclude()) {
+            System.out.println("\nWatch now!");
+        } else {
+            System.out.println("\nUpgrade your plan for watch this!");
+        }
 
     }
 }
---
+++ src/peguidotte/screenmatch/modelos/Film.java
@@ -0,0 +1,110 @@
+package peguidotte.screenmatch.modelos;
+
+public class Film {
+
+    //main
+    private String name;
+    private int date;
+    private String description;
+    private int minutes;
+    private boolean planInclude;
+    private int yearClassification;
+
+    //score
+    private double totalScore;
+    private int scoreAmount;
+
+    //plus
+    private String director;
+    private String genre;
+    private String protagonist;
+
+
+    public int getScoreAmount() {
+        return scoreAmount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public boolean isPlanInclude() {
+        return planInclude;
+    }
+
+    public void setPlanInclude(boolean planInclude) {
+        this.planInclude = planInclude;
+    }
+
+    public int getYearClassification() {
+        return yearClassification;
+    }
+
+    public void setYearClassification(int yearClassification) {
+        this.yearClassification = yearClassification;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getProtagonist() {
+        return protagonist;
+    }
+
+    public void setProtagonist(String protagonist) {
+        this.protagonist = protagonist;
+    }
+
+    public void addScore(double score){
+        totalScore += score;
+        scoreAmount ++;
+    }
+
+    public double averageScore(){
+        return totalScore / scoreAmount;
+    }
+
+
+
+}
